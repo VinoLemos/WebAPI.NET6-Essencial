c102634e5107785b9fa0c38be921e368e8228018
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-08-04 16:36:58 -0300

    First commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using APICatalogo.Models;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {}
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
