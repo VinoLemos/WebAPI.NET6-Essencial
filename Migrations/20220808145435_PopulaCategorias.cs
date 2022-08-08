3da77eff56b65bc5ffc139d02974f5ff838f34f8
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-08-08 17:04:59 -0300

    Migrations Produto e Categoria

---
+++ Migrations/20220808145435_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) values ('Bebidas', 'bebidas.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) values ('Lanches', 'lanches.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) values ('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ Migrations/20220808150526_PopulaProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos" 
+                   + "(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)"
+                   + "values ('Coca-Cola Diet', 'Refrigerante de Cola 350ml', 5.45, 'cocacola.jpg', 50, now(),1)");
+
+            mb.Sql("Insert into Produtos" 
+                   + "(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)"
+                   + "values ('Saunduíche de Atum', 'Sanduíche de Atum com Maionese', 8.50, 'atum.jpg', 10, now(),2)");
+
+            mb.Sql("Insert into Produtos" 
+                   + "(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)"
+                   + "values ('Pudim', 'Pudim de Leite Condensado 100g', 6.75, 'pudim.jpg', 20, now(),3)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
