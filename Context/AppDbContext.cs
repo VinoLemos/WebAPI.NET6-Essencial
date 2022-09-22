94d2384b429997d4b54091ebd552c24a18805c15
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-09-22 14:34:03 -0300

    Contexto & String de conexao

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using AlunosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlunosApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Aluno>? Alunos { get; set; }
+    }
+}
