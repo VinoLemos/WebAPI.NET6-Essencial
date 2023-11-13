9723bfcf5d0bca86d52ac7004e2b74b76468509d
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2023-11-12 21:59:19 -0300

    feat: página de criação de produtos

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -16,6 +17,23 @@ namespace GeekShopping.Web.Controllers
         {
             var products = await _productService.FindAllProducts();
             return View(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(model);
+
+                if (response != null) return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
         }
     }
 }
